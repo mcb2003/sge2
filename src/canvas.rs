1293492679fd85419b07a93228e4c8f14c056c78
mcb2003 <mikey@blindcomputing.org>
2021-08-05 00:53:18 +0100

    Implement many of the canvas drawing functions.
    I've been quite selective in choosing which ones to implement for now.
    Most of the others will follow, but I'll be making subtle changes to
    them to better fit the architecture of SGE.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,33 +1,190 @@
-use sdl2::pixels::Color;
+use sdl2::{
+    pixels::Color,
+    rect::{Point, Rect},
+    render::{BlendMode, Texture},
+};
 
 use crate::{ENGINE, NOT_INIT};
 
-pub fn draw_color() -> Color {
+pub fn blend_mode() -> BlendMode {
     ENGINE.with(|e| {
         let mut engine = e.borrow_mut();
-        let engine = engine
-            .as_mut()
-            .expect(NOT_INIT);
-        engine.canvas.draw_color()
-    })
-}
-
-pub fn set_draw_color<C: Into<Color>>(color: C) {
-    ENGINE.with(|e| {
-        let mut engine = e.borrow_mut();
-        let engine = engine
-            .as_mut()
-            .expect(NOT_INIT);
-        engine.canvas.set_draw_color(color)
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.blend_mode()
     })
 }
 
 pub fn clear() {
     ENGINE.with(|e| {
         let mut engine = e.borrow_mut();
-        let engine = engine
-            .as_mut()
-            .expect(NOT_INIT);
+        let engine = engine.as_mut().expect(NOT_INIT);
         engine.canvas.clear()
+    })
+}
+
+pub fn clip_rect() -> Option<Rect> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.clip_rect()
+    })
+}
+
+pub fn draw_color() -> Color {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_color()
+    })
+}
+
+pub fn draw_line<P1, P2>(start: P1, end: P2) -> Result<(), String>
+where
+P1: Into<Point>,
+P2: Into<Point>,
+{
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_line(start, end)
+    })
+}
+
+pub fn draw_lines<'a, P: Into<&'a [Point]>>(points: P) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_lines(points)
+    })
+}
+
+pub fn draw_point<P: Into<Point>>(point: P) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_point(point)
+    })
+}
+
+pub fn draw_points<'a, P: Into<&'a [Point]>>(points: P) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_points(points)
+    })
+}
+
+pub fn draw_rect<R: Into<Rect>>(rect: R) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_rect(rect.into())
+    })
+}
+
+pub fn draw_rects<'a, R: Into<&'a [Rect]>>(rects: R) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.draw_rects(rects.into())
+    })
+}
+
+pub fn draw_texture<R1, R2>(texture: &Texture<'_>, src: R1, dst: R2) -> Result<(), String>
+where
+    R1: Into<Option<Rect>>,
+    R2: Into<Option<Rect>>,
+{
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.copy(texture, src, dst)
+    })
+}
+
+pub fn draw_texture_ex<R1, R2, P>(
+    texture: &Texture<'_>,
+    src: R1,
+    dst: R2,
+    angle: f64,
+    center: P,
+    flip_horizontal: bool,
+    flip_vertical: bool,
+) -> Result<(), String>
+where
+    R1: Into<Option<Rect>>,
+    R2: Into<Option<Rect>>,
+    P: Into<Option<Point>>,
+{
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.copy_ex(
+            texture,
+            src,
+            dst,
+            angle,
+            center,
+            flip_horizontal,
+            flip_vertical,
+        )
+    })
+}
+
+pub fn fill_rect<R: Into<Rect>>(rect: R) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.fill_rect(rect.into())
+    })
+}
+
+pub fn fill_rects<'a, R: Into<&'a [Rect]>>(rects: R) -> Result<(), String> {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.fill_rects(rects.into())
+    })
+}
+
+
+
+pub fn set_blend_mode(blend: BlendMode) {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.set_blend_mode(blend)
+    })
+}
+
+pub fn set_clip_rect<R: Into<Option<Rect>>>(rect: R) {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.set_clip_rect(rect)
+    })
+}
+
+pub fn set_draw_color<C: Into<Color>>(color: C) {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.set_draw_color(color)
+    })
+}
+
+pub fn set_viewport<R: Into<Option<Rect>>>(rect: R) {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.set_viewport(rect)
+    })
+}
+
+pub fn viewport() -> Rect {
+    ENGINE.with(|e| {
+        let mut engine = e.borrow_mut();
+        let engine = engine.as_mut().expect(NOT_INIT);
+        engine.canvas.viewport()
     })
 }
