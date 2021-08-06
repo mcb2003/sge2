a68f42015e9798f49e9613cbc85abc7460793caf
mcb2003 <mikey@blindcomputing.org>
2021-08-06 11:42:55 +0100

    Panic if a circle's coordinates are out of bounds for an i16
    I wish SDL2_gfx used i32s, but it does not, so this is the best solution for diagnosible error messages.

--- src/gfx.rs
+++ src/gfx.rs
@@ -1,18 +1,22 @@
+use std::convert::TryInto;
+
 use sdl2::gfx::primitives::DrawRenderer;
 
 use crate::{Point, ENGINE, NOT_INIT};
+
+const CIRCLE_X_BOUNDS: &str = "Circle x coordinate out of bounds, must fit in an i16";
+const CIRCLE_Y_BOUNDS: &str = "Circle y coordinate out of bounds, must fit in an i16";
 
 pub fn draw_circle<P: Into<Point>>(center: P, radius: i16) -> Result<(), String> {
     ENGINE.with(|e| {
         let mut engine = e.borrow_mut();
         let engine = engine.as_mut().expect(NOT_INIT);
         let center = center.into();
-        engine.canvas.circle(
-            center.x() as i16,
-            center.y() as i16,
-            radius,
-            engine.canvas.draw_color(),
-        )
+        let x: i16 = center.x().try_into().expect(CIRCLE_X_BOUNDS);
+        let y: i16 = center.y().try_into().expect(CIRCLE_Y_BOUNDS);
+        engine
+            .canvas
+            .circle(x, y, radius, engine.canvas.draw_color())
     })
 }
 
@@ -21,11 +25,10 @@ pub fn fill_circle<P: Into<Point>>(center: P, radius: i16) -> Result<(), String>
         let mut engine = e.borrow_mut();
         let engine = engine.as_mut().expect(NOT_INIT);
         let center = center.into();
-        engine.canvas.filled_circle(
-            center.x() as i16,
-            center.y() as i16,
-            radius,
-            engine.canvas.draw_color(),
-        )
+        let x: i16 = center.x().try_into().expect(CIRCLE_X_BOUNDS);
+        let y: i16 = center.y().try_into().expect(CIRCLE_Y_BOUNDS);
+        engine
+            .canvas
+            .filled_circle(x, y, radius, engine.canvas.draw_color())
     })
 }
