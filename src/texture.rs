0f3e30aaced636bf31d224b773b9435018322ef4
mcb2003 <mikey@blindcomputing.org>
2021-08-19 12:56:18 +0100

    Update existing Texture code to use lifetimeless Textures.

--- src/texture.rs
+++ src/texture.rs
@@ -1,11 +1,10 @@
-/*
 use std::path::Path;
 
 use sdl2::surface::Surface;
 
 use crate::{Texture, ENGINE, NOT_INIT};
 
-pub fn create_texture<P: AsRef<Path>>(file_path: P) -> Result<Texture<'static>, String> {
+pub fn create_texture<P: AsRef<Path>>(file_path: P) -> Result<Texture, String> {
     let surface = Surface::load_bmp(file_path)?;
 
     ENGINE.with(move |e| {
@@ -13,4 +12,3 @@ pub fn create_texture<P: AsRef<Path>>(file_path: P) -> Result<Texture<'static>, 
         surface.as_texture(&mut engine.texture_creator).map_err(|e| e.to_string())
     })
 }
-*/
