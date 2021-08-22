cc6994454a1e85d474e7b39d243df2e63f11c360
mcb2003 <mikey@blindcomputing.org>
2021-08-22 15:46:49 +0100

    Clean up moving_texture example.

--- examples/moving_texture.rs
+++ examples/moving_texture.rs
@@ -14,7 +14,11 @@ struct App {
 
 impl App {
     pub fn new() -> Self {
-        Self { tex: None, x: 10.0, y: 10.0 }
+        Self {
+            tex: None,
+            x: 10.0,
+            y: 10.0,
+        }
     }
 }
 
@@ -25,32 +29,43 @@ impl sge::Application for App {
     }
 
     fn on_update(&mut self, elapsed_time: f64) -> sge::ApplicationResult {
-        let tex = self.tex.as_ref().unwrap();
-        // Move the texture with the keyboard
-        if sge::key(Scancode::Up).held {
-            self.y = (self.y - MOVEMENT_SPEED * elapsed_time).max(0.0);
-        } else if sge::key(Scancode::Down).held {
-            self.y =
-                (self.y + MOVEMENT_SPEED * elapsed_time).min((SCREEN_HEIGHT - tex.size().y() as u32) as f64);
-        }
-        if sge::key(Scancode::Left).held {
-            self.x = (self.x - MOVEMENT_SPEED * elapsed_time).max(0.0);
-        } else if sge::key(Scancode::Right).held {
-            self.x =
-                (self.x + MOVEMENT_SPEED * elapsed_time).min((SCREEN_WIDTH - tex.size().x() as u32) as f64);
-        }
-        // Move the texture with the mouse
-        if sge::mouse_button(MouseButton::Left).held {
-            let pos = sge::mouse_pos();
-            self.x = pos.x() as f64;
-            self.y = pos.y() as f64;
-        }
-        // Fill the screen
         sge::clear(Color::BLACK);
-        tex.draw(
-            None,
- Rect::new(self.x as i32, self.y as i32, tex.size().x() as u32, tex.size().y() as u32),
-        )?;
+
+        if let Some(ref tex) = self.tex {
+            let size = tex.size();
+            let size = (size.x() as u32, size.y() as u32);
+
+            // Move the texture with the keyboard
+            if sge::key(Scancode::Up).held {
+                self.y = (self.y - MOVEMENT_SPEED * elapsed_time).max(0.0);
+            } else if sge::key(Scancode::Down).held {
+                self.y =
+                    (self.y + MOVEMENT_SPEED * elapsed_time).min((SCREEN_HEIGHT - size.1) as f64);
+            }
+            if sge::key(Scancode::Left).held {
+                self.x = (self.x - MOVEMENT_SPEED * elapsed_time).max(0.0);
+            } else if sge::key(Scancode::Right).held {
+                self.x =
+                    (self.x + MOVEMENT_SPEED * elapsed_time).min((SCREEN_WIDTH - size.0) as f64);
+            }
+
+            // Move the texture with the mouse
+            if sge::mouse_button(MouseButton::Left).held {
+                let pos = sge::mouse_pos();
+                self.x = pos.x() as f64;
+                self.y = pos.y() as f64;
+            }
+
+            tex.draw(
+                None, // Src rect (None = entire texture)
+                Rect::new(
+                    self.x as i32,
+                    self.y as i32,
+                    tex.size().x() as u32,
+                    tex.size().y() as u32,
+                ),
+            )?;
+        }
         Ok(true)
     }
 }
