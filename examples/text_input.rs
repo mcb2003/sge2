845082f503778bf6baef142f5c3c12b2aec52ec8
mcb2003 <mikey@blindcomputing.org>
2021-08-30 11:39:57 +0100

    Add a very basic example of text input.
    This doesn't yet handle IMEs / text composition, but it works for latin scripts at least.

---
+++ examples/text_input.rs
@@ -0,0 +1,50 @@
+use std::error::Error;
+
+use sge::prelude::*;
+
+const SCREEN_WIDTH: u32 = 480;
+const SCREEN_HEIGHT: u32 = 360;
+
+struct App {
+    text: String,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self { text: String::new() }
+    }
+}
+
+impl sge::Application for App {
+    fn on_update(&mut self, elapsed_time: f64) -> sge::ApplicationResult {
+        if sge::key(Scancode::Backspace).pressed {
+            self.text.pop();
+        }
+        if sge::key(Scancode::Return).pressed {
+            self.text.push('\n');
+        }
+        sge::clear(Color::BLACK);
+        for (i, line) in self.text.lines().enumerate() {
+            // Note: this doesn't wrap lines, they'll just be cut off
+        sge::draw_string((0, 8 * i as i32), line, Color::WHITE);
+        }
+        Ok(true)
+    }
+
+    fn on_event(&mut self, event: &sge::Event) -> sge::ApplicationResult {
+        use sge::Event;
+
+        match event {
+            Event::TextInput { text, .. } => {
+                self.text.push_str(&text);
+                Ok(true) // We handled this event
+            },
+            _ => Ok(false), // Unhandled event
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut app = App::new();
+    sge::Builder::new("Moving Rectangle", SCREEN_WIDTH, SCREEN_HEIGHT).start(&mut app)
+}
